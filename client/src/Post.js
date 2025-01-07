3f750a4f71d3e8473b3bfbb89cd2783803ac9dc2
Kelven <kelvinafutu8as@gmail.com>
2025-01-07 15:20:46 +0000

    30mins

---
+++ client/src/Post.js
@@ -0,0 +1,26 @@
+export default  function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2024/04/IMG_0977.jpg?resize=668,501"
+          alt=""
+        />
+        </div>
+
+        <div className="text">
+        <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+        <p className="info">
+          <a className="author">David Pacca</a>
+          <time>2023-01-08 16:45</time>
+        </p>
+        <p className="summary">
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.
+        </p>
+        </div>
+        
+      </div>
+    )
+}
